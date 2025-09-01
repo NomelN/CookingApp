4fe4e7a85dcc1e13d4cd21785b0f909d733c9c3b
NomelN <akpanomel@gmail.com>
2025-09-01 15:47:58 +0200

    Initial Commit

---
+++ CookingApp/CookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingAppApp.swift
+//  CookingApp
+//
+//  Created by Mickaël Nomel on 01/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CookingAppTests/CookingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CookingAppTests.swift
+//  CookingAppTests
+//
+//  Created by Mickaël Nomel on 01/09/2025.
+//
+
+import Testing
+@testable import CookingApp
+
+struct CookingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
